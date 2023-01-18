fdb0265ab9afb9ff73311cca1bfacbbf4cfebd69
Anisha0906 <anishadas880@gmail.com>
2023-01-18 22:39:25 +0530

    [Anisha]Added:Create a simple linkedlist of 56,30,70

---
+++ LinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist list = new Linkedlist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.ReadLine();
+        }
+    }
+}
